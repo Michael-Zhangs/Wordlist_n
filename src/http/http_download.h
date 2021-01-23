9eb6a8840b2e433066547904781eb464e0c496a6
Michael-Zhang <Michael_Zhangs@163.com>
2021-01-23 18:57:20 +0800

    added update system

---
+++ src/http/http_download.h
@@ -0,0 +1,6 @@
+#ifndef HTTP_DOWNLOAD_H
+#define	HTTP_DOWNLOAD_H
+
+int http_download(char *url, char *save_path);
+
+#endif
---
+++ src/setup.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "http/http_download.h"
+#define BUF_SIZE 1024 // the version length must be smaller than buf_size
+
+int checkversion()
+{
+    http_download("http://1149e7e4.nat5.nsloop.com/version","../tmp/version");
+    int openFlags;
+    char prebuf[BUF_SIZE],onlbuf[BUF_SIZE];
+    openFlags = O_RDONLY;
+    int fd;
+    fd=open("version",openFlags);
+    if(read(fd,prebuf,BUF_SIZE)==-1)
+        printf("ERROR while reading the version\n");
+    if(close(fd)==-1)
+        printf("ERROR while closing the file\n");
+    fd=open("../tmp/version",openFlags);
+    if(read(fd,onlbuf,BUF_SIZE)==-1)
+        printf("ERROR while reading the version\n");
+    if(close(fd)==-1)
+        printf("ERROR while closing the file\n");
+    for(int a=0;a<BUF_SIZE;a++)
+    {
+        if(prebuf[a]!=onlbuf[a])
+            return 1;
+    }
+    return 0;
+}
+
+int update()
+{
+    if(http_download("http://1149e7e4.nat5.nsloop.com/wordlist","wordlist")==-1)
+        return -1;
+    http_download("http://1149e7e4.nat5.nsloop.com/version","version");
+    return 0;
+}
+
+int main()
+{
+    if(checkversion()==1)
+    {
+        printf("Not the lastest version,updating\n");
+        if(update()==-1)
+            printf("Update error\n");
+    }
+    execve("wordlist",0,0);
+    return 0;
+}
